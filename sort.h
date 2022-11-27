9199dae9f45fdea43403a8dbee6fa755b75353b9
jjenus <biz.jenus@gmail.com>
2022-11-27 05:13:26 +0000

    fixed sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef _SORT_H
 #define _SORT_H
-
 #include <stddef.h>
 
 /**
@@ -12,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
